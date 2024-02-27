da3170ca9439c31f87e796ea4bc87ca496fc02cb
Manikanta662001 <gundlurimanikanta142@gmail.com>
2024-02-27 11:26:39 +0530

    implemente login api call,Added basename in browserrouter and setting of cookie

--- src/api/Service.jsx
+++ src/api/Service.jsx
@@ -1,20 +1,20 @@
-const post = async(url,data)=>{
-    try{
-        const apiResponse = await fetch(url,{
-            method:"POST",
-            headers:{
-              "Content-Type" : "application/json"
-            },
-            body:JSON.stringify(data)
-          })
-          const result = await apiResponse.json();
-          if (!apiResponse.ok){
-            throw new Error(result.error)
-          }
-          return result;
-
-    }catch(error){
-        throw new Error(error.message);
+const post = async (url, data) => {
+  try {
+    const apiResponse = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(apiResponse, "10");
+    const result = await apiResponse.json();
+    if (!apiResponse.ok) {
+      throw new Error(result.error);
     }
-}
-export const httpMethods = {post}
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+export const httpMethods = { post };
